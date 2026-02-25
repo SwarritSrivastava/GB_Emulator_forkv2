e05d5fec29b4d38665588adf893bf3fa5b4ceaeb
Jayesh Puri <jayeshkpuri@gmail.com>
2026-02-25 13:30:30 +0530

    success header initialized

---
+++ include/success.hpp
@@ -0,0 +1,11 @@
+//
+// Created by wizard on 25/02/26.
+//
+
+#pragma once
+
+void success(){
+    std::cout<<"--------------------"<<std::endl;
+    std::cout<<"      Success!      "<<std::endl;
+    std::cout<<"--------------------"<<std::endl;
+}
