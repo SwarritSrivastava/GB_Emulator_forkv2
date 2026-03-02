6a08cb9ef7413dbefac8041a41e96a8a584387a8
Jayesh Puri <jayeshkpuri@gmail.com>
2026-03-02 16:26:44 +0530

    update

--- src/main.cpp
+++ src/main.cpp
@@ -34,13 +34,13 @@ int main(const int argc, char **argv)
         std::cout << "Initial State (Post-Reset):" << std::endl;
         cpu.printStatus();
         success();
-        int opcode_count = 0;
+        int con = 0;
         while (!cpu.isHalt())
         {
-            opcode_count++;
+            con++;
             cpu.step(mmu);
         }
-        std::cout << std::dec << std::endl << opcode_count  << " opcodes printed" << std::endl;
+        std::cout << std::dec << std::endl << con  << " instructions executed" << std::endl;
     }
     catch (const std::exception &e)
     {
