737a8f1137a09dad83e35e5baa2a90d780b286c0
Jayesh Puri <jayeshkpuri@gmail.com>
2026-03-21 15:58:33 +0530

    added opcodes

--- src/core/cpu/instructions/opcodes/op_01.cpp
+++ src/core/cpu/instructions/opcodes/op_01.cpp
@@ -24,11 +24,52 @@ int op_ld_de_d16(ProcessingUnit &cpu, MMU &mmu) // 0x11
     return totalMachineCycles(3);
 }
 
-DUMMY(op_ld_de_a) // 0x12
-DUMMY(op_inc_de) // 0x13
-DUMMY(op_inc_d) // 0x14
-DUMMY(op_dec_d) // 0x15
-DUMMY(op_ld_d_d8) // 0x16
+int op_ld_de_a(ProcessingUnit &cpu, MMU &mmu) // 0x12
+{
+    const u16 addr = cpu.get_de();      // DE contains the target address
+    const u8 value = cpu.reg(ProcessingUnit::Register::A);
+
+    mmu.write(addr, value);
+
+    return totalMachineCycles(2);
+}
+
+int op_inc_de(ProcessingUnit& cpu, MMU& mmu) // 0x13
+{
+    const u16 newValue = cpu.get_de() + 1;
+
+    cpu.reg(ProcessingUnit::Register::D) = (newValue >> 8) & 0xFF;
+    cpu.reg(ProcessingUnit::Register::E) = newValue & 0xFF;
+
+    return totalMachineCycles(2);
+}
+
+int op_inc_d(ProcessingUnit& cpu, MMU& mmu) // 0x14
+{
+    const u8 newValue = cpu.reg(ProcessingUnit::Register::D) + 1;
+    cpu.reg(ProcessingUnit::Register::D) = newValue;
+
+    return totalMachineCycles(1);
+}
+
+int op_dec_d(ProcessingUnit& cpu, MMU& mmu) // 0x15
+{
+    const u8 newValue = cpu.reg(ProcessingUnit::Register::D) - 1;
+    cpu.reg(ProcessingUnit::Register::D) = newValue;
+
+    return totalMachineCycles(1);
+}
+
+int op_ld_d_d8(ProcessingUnit& cpu, MMU& mmu) // 0x16
+{
+    cpu.inc_pc();
+    const u8 newValue = mmu.read(cpu.inc_pc());
+
+    cpu.reg(ProcessingUnit::Register::D) = newValue;
+
+    return totalMachineCycles(2);
+}
+
 DUMMY(op_rla) // 0x17
 DUMMY(op_jr_r8) // 0x18
 DUMMY(op_add_hl_de) // 0x19
