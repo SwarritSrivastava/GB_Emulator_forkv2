d04ffbd454b660928057e64e69010d8f0bd7c048
SwarritSrivastava <swarit.srivastava2006@gmail.com>
2026-03-26 00:03:48 +0530

    Added test for CB_Prefix

--- tests/op_tests/op_0C_tests.cpp
+++ tests/op_tests/op_0C_tests.cpp
@@ -2,9 +2,32 @@
 #include "../../include/mmu.hpp"
 #include "../../include/ProcessingUnit.hpp"
 #include "../../include/opcodes.hpp"
+#include "../../include/opcode_table.hpp"
 
 class OpcodesCPUTest : public testing::Test {
 protected:
     ProcessingUnit cpu;
     MMU mmu;
 };
+
+TEST_F(OpcodesCPUTest, CB_Prefix) 
+{
+    const u8 test_opcode = 0x00;
+    const u16 start_pc = 0x100;
+
+    cpu.set_pc(start_pc);
+    mmu.write(start_pc, test_opcode);
+
+    auto original = cbInstructionTable[test_opcode];
+    cbInstructionTable[test_opcode] = [](ProcessingUnit&, MMU&) -> int 
+    {
+        return 2;
+    };
+
+    int cycles = op_cb_prefix(cpu, mmu);
+
+    EXPECT_EQ(cycles, 2);
+    EXPECT_EQ(cpu.get_pc(), start_pc + 1);
+
+    cbInstructionTable[test_opcode] = original;
+}
