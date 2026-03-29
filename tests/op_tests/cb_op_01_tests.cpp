1f38550e5c2252932b9d2ec513c29bbe2ce58e0b
SwarritSrivastava <swarit.srivastava2006@gmail.com>
2026-03-29 23:32:04 +0530

    feat: ADDED RL_B for CB01 and added TEST

--- tests/op_tests/cb_op_01_tests.cpp
+++ tests/op_tests/cb_op_01_tests.cpp
@@ -10,3 +10,27 @@ protected:
     MMU mmu;
 };
 
+TEST_F(CB_OpcodesCPUTest, RLB_RotatesBRegisterLeft) {
+    cpu.reg(ProcessingUnit::Register::B) = 0x80;
+    cpu.setFlag(ProcessingUnit::Flag::C , false);
+
+    const int c0 = op_rl_b(cpu, mmu);
+
+    EXPECT_EQ(cpu.reg(ProcessingUnit::Register::B) , 0x00);
+    EXPECT_TRUE(cpu.get_flag_c());
+    EXPECT_TRUE(cpu.get_flag_z());
+    EXPECT_FALSE(cpu.get_flag_n());
+    EXPECT_FALSE(cpu.get_flag_h());
+    EXPECT_EQ(c0, 8);
+
+    cpu.reg(ProcessingUnit::Register::B) = 0x01;
+    cpu.setFlag(ProcessingUnit::Flag::C , true);
+
+    op_rl_b(cpu, mmu);
+
+    EXPECT_EQ(cpu.reg(ProcessingUnit::Register::B), 0x03);
+    EXPECT_FALSE(cpu.get_flag_c());
+    EXPECT_FALSE(cpu.get_flag_z());
+    EXPECT_FALSE(cpu.get_flag_n());
+    EXPECT_FALSE(cpu.get_flag_h());
+}
