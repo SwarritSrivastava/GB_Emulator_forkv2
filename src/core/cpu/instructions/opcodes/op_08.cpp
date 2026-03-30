1b430ed990040b8f3d845961ec18bd2aefdca1d4
Jayesh Puri <jayeshkpuri@gmail.com>
2026-03-30 22:09:37 +0530

    added opcode 0x82 and 0x83 - tests pending

--- src/core/cpu/instructions/opcodes/op_08.cpp
+++ src/core/cpu/instructions/opcodes/op_08.cpp
@@ -40,12 +40,45 @@ int op_add_a_c(ProcessingUnit& cpu, MMU& mmu) // 0x81
     return totalMachineCycles(1);
 }
 
-DUMMY(op_add_a_d) // 0x82
-DUMMY(op_add_a_e) // 0x83
+int op_add_a_d(ProcessingUnit& cpu, MMU& mmu) // 0x82
+{
+    const u8 a = cpu.reg(ProcessingUnit::Register::A);
+    const u8 d = cpu.reg(ProcessingUnit::Register::D);
+    const u16 sum = a + d;
+
+    const u8 result = static_cast<u8>(sum & 0xFF);
+    cpu.reg(ProcessingUnit::Register::A) = result;
+
+    cpu.setFlag(ProcessingUnit::Flag::Z, result == 0);
+    cpu.setFlag(ProcessingUnit::Flag::N, false);
+    cpu.setFlag(ProcessingUnit::Flag::H, ((a & 0x0F) + (d & 0x0F)) > 0x0F);
+    cpu.setFlag(ProcessingUnit::Flag::C, sum > 0xFF);
+
+    return totalMachineCycles(1);
+}
+
+int op_add_a_e(ProcessingUnit& cpu, MMU& mmu) // 0x83
+{
+    const u8 a = cpu.reg(ProcessingUnit::Register::A);
+    const u8 e = cpu.reg(ProcessingUnit::Register::E);
+    const u16 sum = a + e;
+
+    const u8 result = static_cast<u8>(sum & 0xFF);
+    cpu.reg(ProcessingUnit::Register::A) = result;
+
+    cpu.setFlag(ProcessingUnit::Flag::Z, result == 0);
+    cpu.setFlag(ProcessingUnit::Flag::N, false);
+    cpu.setFlag(ProcessingUnit::Flag::H, ((a & 0x0F) + (e & 0x0F)) > 0x0F);
+    cpu.setFlag(ProcessingUnit::Flag::C, sum > 0xFF);
+
+    return totalMachineCycles(1);
+}
+
 DUMMY(op_add_a_h) // 0x84
 DUMMY(op_add_a_l) // 0x85
 DUMMY(op_add_a_hl) // 0x86
 DUMMY(op_add_a_a) // 0x87
+
 DUMMY(op_adc_a_b) // 0x88
 DUMMY(op_adc_a_c) // 0x89
 DUMMY(op_adc_a_d) // 0x8A
