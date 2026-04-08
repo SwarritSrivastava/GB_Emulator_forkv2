d55e8f73600dfb16f229723281f764219e102206
SwarritSrivastava <swarit.srivastava2006@gmail.com>
2026-04-08 22:51:06 +0530

    fix: added cycle verification for cB2 opcodes

--- tests/op_tests/cb_op_02_tests.cpp
+++ tests/op_tests/cb_op_02_tests.cpp
@@ -12,13 +12,15 @@ protected:
 
 TEST_F(CB_OpcodesCPUTest, SLA_B) {
     cpu.reg(ProcessingUnit::Register::B) = 0x80;
-    op_sla_b(cpu, mmu);
+    int cycles = op_sla_b(cpu, mmu);
+    EXPECT_EQ(cycles, 8);
     EXPECT_EQ(cpu.reg(ProcessingUnit::Register::B), 0x00);
     EXPECT_TRUE(cpu.get_flag_c());
     EXPECT_TRUE(cpu.get_flag_z());
 
     cpu.reg(ProcessingUnit::Register::B) = 0x45;
-    op_sla_b(cpu, mmu);
+    cycles = op_sla_b(cpu, mmu);
+    EXPECT_EQ(cycles, 8);
     EXPECT_EQ(cpu.reg(ProcessingUnit::Register::B), 0x8A);
     EXPECT_FALSE(cpu.get_flag_c());
     EXPECT_FALSE(cpu.get_flag_z());
@@ -26,7 +28,8 @@ TEST_F(CB_OpcodesCPUTest, SLA_B) {
 
 TEST_F(CB_OpcodesCPUTest, SLA_C) {
     cpu.reg(ProcessingUnit::Register::C) = 0x80;
-    op_sla_c(cpu, mmu);
+    int cycles = op_sla_c(cpu, mmu);
+    EXPECT_EQ(cycles, 8);
     EXPECT_EQ(cpu.reg(ProcessingUnit::Register::C), 0x00);
     EXPECT_TRUE(cpu.get_flag_c());
     EXPECT_TRUE(cpu.get_flag_z());
@@ -34,7 +37,8 @@ TEST_F(CB_OpcodesCPUTest, SLA_C) {
 
 TEST_F(CB_OpcodesCPUTest, SLA_D) {
     cpu.reg(ProcessingUnit::Register::D) = 0x80;
-    op_sla_d(cpu, mmu);
+    int cycles = op_sla_d(cpu, mmu);
+    EXPECT_EQ(cycles, 8);
     EXPECT_EQ(cpu.reg(ProcessingUnit::Register::D), 0x00);
     EXPECT_TRUE(cpu.get_flag_c());
     EXPECT_TRUE(cpu.get_flag_z());
@@ -42,7 +46,8 @@ TEST_F(CB_OpcodesCPUTest, SLA_D) {
 
 TEST_F(CB_OpcodesCPUTest, SLA_E) {
     cpu.reg(ProcessingUnit::Register::E) = 0x80;
-    op_sla_e(cpu, mmu);
+    int cycles = op_sla_e(cpu, mmu);
+    EXPECT_EQ(cycles, 8);
     EXPECT_EQ(cpu.reg(ProcessingUnit::Register::E), 0x00);
     EXPECT_TRUE(cpu.get_flag_c());
     EXPECT_TRUE(cpu.get_flag_z());
@@ -50,7 +55,8 @@ TEST_F(CB_OpcodesCPUTest, SLA_E) {
 
 TEST_F(CB_OpcodesCPUTest, SLA_H) {
     cpu.reg(ProcessingUnit::Register::H) = 0x80;
-    op_sla_h(cpu, mmu);
+    int cycles = op_sla_h(cpu, mmu);
+    EXPECT_EQ(cycles, 8);
     EXPECT_EQ(cpu.reg(ProcessingUnit::Register::H), 0x00);
     EXPECT_TRUE(cpu.get_flag_c());
     EXPECT_TRUE(cpu.get_flag_z());
@@ -58,7 +64,8 @@ TEST_F(CB_OpcodesCPUTest, SLA_H) {
 
 TEST_F(CB_OpcodesCPUTest, SLA_L) {
     cpu.reg(ProcessingUnit::Register::L) = 0x80;
-    op_sla_l(cpu, mmu);
+    int cycles = op_sla_l(cpu, mmu);
+    EXPECT_EQ(cycles, 8);
     EXPECT_EQ(cpu.reg(ProcessingUnit::Register::L), 0x00);
     EXPECT_TRUE(cpu.get_flag_c());
     EXPECT_TRUE(cpu.get_flag_z());
@@ -69,7 +76,8 @@ TEST_F(CB_OpcodesCPUTest, SLA_HL) {
     cpu.reg(ProcessingUnit::Register::H) = (addr >> 8) & 0xFF;
     cpu.reg(ProcessingUnit::Register::L) = addr & 0xFF;
     mmu.write(addr, 0x81);
-    op_sla_hl(cpu, mmu);
+    int cycles = op_sla_hl(cpu, mmu);
+    EXPECT_EQ(cycles, 16);
     EXPECT_EQ(mmu.read(addr), 0x02);
     EXPECT_TRUE(cpu.get_flag_c());
     EXPECT_FALSE(cpu.get_flag_z());
@@ -77,7 +85,8 @@ TEST_F(CB_OpcodesCPUTest, SLA_HL) {
 
 TEST_F(CB_OpcodesCPUTest, SLA_A) {
     cpu.reg(ProcessingUnit::Register::A) = 0x80;
-    op_sla_a(cpu, mmu);
+    int cycles = op_sla_a(cpu, mmu);
+    EXPECT_EQ(cycles, 8);
     EXPECT_EQ(cpu.reg(ProcessingUnit::Register::A), 0x00);
     EXPECT_TRUE(cpu.get_flag_c());
     EXPECT_TRUE(cpu.get_flag_z());
@@ -85,13 +94,15 @@ TEST_F(CB_OpcodesCPUTest, SLA_A) {
 
 TEST_F(CB_OpcodesCPUTest, SRA_B) {
     cpu.reg(ProcessingUnit::Register::B) = 0x81;
-    op_sra_b(cpu, mmu);
+    int cycles = op_sra_b(cpu, mmu);
+    EXPECT_EQ(cycles, 8);
     EXPECT_EQ(cpu.reg(ProcessingUnit::Register::B), 0xC0);
     EXPECT_TRUE(cpu.get_flag_c());
     EXPECT_FALSE(cpu.get_flag_z());
 
     cpu.reg(ProcessingUnit::Register::B) = 0x01;
-    op_sra_b(cpu, mmu);
+    cycles = op_sra_b(cpu, mmu);
+    EXPECT_EQ(cycles, 8);
     EXPECT_EQ(cpu.reg(ProcessingUnit::Register::B), 0x00);
     EXPECT_TRUE(cpu.get_flag_c());
     EXPECT_TRUE(cpu.get_flag_z());
@@ -99,35 +110,40 @@ TEST_F(CB_OpcodesCPUTest, SRA_B) {
 
 TEST_F(CB_OpcodesCPUTest, SRA_C) {
     cpu.reg(ProcessingUnit::Register::C) = 0x81;
-    op_sra_c(cpu, mmu);
+    int cycles = op_sra_c(cpu, mmu);
+    EXPECT_EQ(cycles, 8);
     EXPECT_EQ(cpu.reg(ProcessingUnit::Register::C), 0xC0);
     EXPECT_TRUE(cpu.get_flag_c());
 }
 
 TEST_F(CB_OpcodesCPUTest, SRA_D) {
     cpu.reg(ProcessingUnit::Register::D) = 0x81;
-    op_sra_d(cpu, mmu);
+    int cycles = op_sra_d(cpu, mmu);
+    EXPECT_EQ(cycles, 8);
     EXPECT_EQ(cpu.reg(ProcessingUnit::Register::D), 0xC0);
     EXPECT_TRUE(cpu.get_flag_c());
 }
 
 TEST_F(CB_OpcodesCPUTest, SRA_E) {
     cpu.reg(ProcessingUnit::Register::E) = 0x81;
-    op_sra_e(cpu, mmu);
+    int cycles = op_sra_e(cpu, mmu);
+    EXPECT_EQ(cycles, 8);
     EXPECT_EQ(cpu.reg(ProcessingUnit::Register::E), 0xC0);
     EXPECT_TRUE(cpu.get_flag_c());
 }
 
 TEST_F(CB_OpcodesCPUTest, SRA_H) {
     cpu.reg(ProcessingUnit::Register::H) = 0x81;
-    op_sra_h(cpu, mmu);
+    int cycles = op_sra_h(cpu, mmu);
+    EXPECT_EQ(cycles, 8);
     EXPECT_EQ(cpu.reg(ProcessingUnit::Register::H), 0xC0);
     EXPECT_TRUE(cpu.get_flag_c());
 }
 
 TEST_F(CB_OpcodesCPUTest, SRA_L) {
     cpu.reg(ProcessingUnit::Register::L) = 0x81;
-    op_sra_l(cpu, mmu);
+    int cycles = op_sra_l(cpu, mmu);
+    EXPECT_EQ(cycles, 8);
     EXPECT_EQ(cpu.reg(ProcessingUnit::Register::L), 0xC0);
     EXPECT_TRUE(cpu.get_flag_c());
 }
@@ -137,7 +153,8 @@ TEST_F(CB_OpcodesCPUTest, SRA_HL) {
     cpu.reg(ProcessingUnit::Register::H) = (addr >> 8) & 0xFF;
     cpu.reg(ProcessingUnit::Register::L) = addr & 0xFF;
     mmu.write(addr, 0x80);
-    op_sra_hl(cpu, mmu);
+    int cycles = op_sra_hl(cpu, mmu);
+    EXPECT_EQ(cycles, 16);
     EXPECT_EQ(mmu.read(addr), 0xC0);
     EXPECT_FALSE(cpu.get_flag_c());
     EXPECT_FALSE(cpu.get_flag_z());
@@ -145,7 +162,8 @@ TEST_F(CB_OpcodesCPUTest, SRA_HL) {
 
 TEST_F(CB_OpcodesCPUTest, SRA_A) {
     cpu.reg(ProcessingUnit::Register::A) = 0x81;
-    op_sra_a(cpu, mmu);
+    int cycles = op_sra_a(cpu, mmu);
+    EXPECT_EQ(cycles, 8);
     EXPECT_EQ(cpu.reg(ProcessingUnit::Register::A), 0xC0);
     EXPECT_TRUE(cpu.get_flag_c());
 }
