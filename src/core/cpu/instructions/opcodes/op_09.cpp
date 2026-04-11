d24ab14f1defb4f7afa52cdd299d654b6a2c2e72
Tanishq Singh <hello@tanishqsingh.com>
2026-04-11 17:11:09 +0530

    fix: opcode 0x90

--- src/core/cpu/instructions/opcodes/op_09.cpp
+++ src/core/cpu/instructions/opcodes/op_09.cpp
@@ -22,3 +22,19 @@ int op_sub_b(ProcessingUnit &cpu, MMU &mmu) // 0x90
     cpu.reg(ProcessingUnit::Register::A) = static_cast<u8>(result);
     return totalMachineCycles(1);
 }
+
+DUMMY(op_sub_c)    // 0x91
+DUMMY(op_sub_d)    // 0x92
+DUMMY(op_sub_e)    // 0x93
+DUMMY(op_sub_h)    // 0x94
+DUMMY(op_sub_l)    // 0x95
+DUMMY(op_sub_hl)   // 0x96
+DUMMY(op_sub_a)    // 0x97
+DUMMY(op_sbc_a_b)  // 0x98
+DUMMY(op_sbc_a_c)  // 0x99
+DUMMY(op_sbc_a_d)  // 0x9A
+DUMMY(op_sbc_a_e)  // 0x9B
+DUMMY(op_sbc_a_h)  // 0x9C
+DUMMY(op_sbc_a_l)  // 0x9D
+DUMMY(op_sbc_a_hl) // 0x9E
+DUMMY(op_sbc_a_a)  // 0x9F
