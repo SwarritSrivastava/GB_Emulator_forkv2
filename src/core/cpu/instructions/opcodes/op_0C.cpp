4050b9d2bcca437a537c39dc75a396884108bd42
Jayesh Puri <jayeshkpuri@gmail.com>
2026-04-24 23:52:35 +0530

    op0xca - jmp_nz|flag(!)|jmp_z

--- src/core/cpu/instructions/opcodes/op_0C.cpp
+++ src/core/cpu/instructions/opcodes/op_0C.cpp
@@ -60,7 +60,21 @@ DUMMY(op_add_a_d8) // 0xC6
 DUMMY(op_rst_00) // 0xC7
 DUMMY(op_ret_z) // 0xC8
 DUMMY(op_ret) // 0xC9
-DUMMY(op_jp_z) // 0xCA
+
+int op_jp_z(ProcessingUnit& cpu, MMU& mmu) // 0xCA
+{
+    const u8 lo = mmu.read(cpu.inc_pc());
+    const u8 hi = mmu.read(cpu.inc_pc());
+    const u16 addr = static_cast<u16>((hi << 8) | lo);
+
+    if (cpu.get_flag_z())
+    {
+        cpu.set_pc(addr);
+        return totalMachineCycles(4);
+    }
+
+    return totalMachineCycles(3);
+}
 
 int op_cb_prefix(ProcessingUnit& cpu , MMU& mmu) // 0xCB
 {
